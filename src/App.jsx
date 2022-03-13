8fb483ee12ce9b7cf0515f9093f1a104a820563f
Jishkarianii <jishkarianigigi9@gmail.com>
2022-03-14 00:07:27 +0400

    added count down timer function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.scss'
 import { useState } from 'react'
 
+let timeInterval;
+
 function App() {
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
+  const [timer, setTimer] = useState("25:00")
+  const [timeOut, setTimeOut] = useState(false)
 
   const incrementBreak = () => {
     if (breakLength === 60) return
@@ -28,6 +32,48 @@ function App() {
     
     setSessionLength(sessionLength - 1)
   }
+
+  const countDownTimer = () => {
+    // Selected minute and second
+    let min = timer.substring(0, 2)
+    let sec = timer.substring(3, 5)
+    let tempTimer;
+
+    timeInterval = setInterval(() => {
+      // Check if timer second is 0
+      if (sec === "00") {
+        min--;
+
+        // If minute is less than 10 then correct it 
+        if (min < 10) {
+          min = "0" + min;
+        }
+        
+        sec = "60";
+      }
+
+      // Add class "time-out" if time is less than 1 minute
+      if (min === "00") {
+        setTimeOut(true)
+      }
+
+      sec--;
+      
+      // If second is less than 10 then correct it 
+      if (sec < 10) {
+        sec = "0" + sec;
+      }
+
+      // If time is out
+      if (tempTimer === "00:01") {
+        clearInterval(timeInterval)
+      }
+
+      tempTimer = `${min}:${sec}`;
+      setTimer(`${min}:${sec}`)
+    }, 1000);
+  }
+
 
   return (
     <div className="App">
@@ -69,18 +115,18 @@ function App() {
         </div>
         <div className="clock-timer">
           <p id="timer-label">Session</p>
-          <p id="time-left">25:00</p>
+          <p id="time-left" className={timeOut ? "time-out" : null}>{timer}</p>
         </div>
         <div className="timer-control">
           <i 
             id="start_stop"
             className="fa fa-play fa-2x" 
-            onClick={null}
+            onClick={countDownTimer}
           />
           <i 
             id="start_stop"
             className="fa fa-pause fa-2x" 
-            onClick={null}
+            onClick={() => clearInterval(timeInterval)}
           />
           <i 
             id="reset"
